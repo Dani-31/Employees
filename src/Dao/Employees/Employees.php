bf061eee7a5c1befd1b67be75142a975f963ed7c
Dani-31 <rodriguezdani559@gmail.com>
2024-08-08 21:59:48 -0600

    Nuevo

--- src/Dao/Employees/Employees.php
+++ src/Dao/Employees/Employees.php
@@ -4,8 +4,30 @@ namespace Dao\Employees;
 
 class Employees extends \Dao\Table
 {
+    
+         /**Nuevo*/
 
-
+    public static function createEmployee(
+        $FirstName,
+        $LastName,
+        $HireDate,
+        $DepartmentID
+        ) {
+            $InsSql = "INSERT INTO Employees (FirstName, LastName, HireDate,DepartmentID)
+             value (:FirstName, :LastName, :HireDate, :DepartmentID);";
+            $insParams = [
+              
+                'FirstName' => $FirstName,
+                'LastName' => $LastName,
+                'HireDate' => $HireDate,
+                'DepartmentID' => $DepartmentID
+           
+    
+            ];
+    
+            return self::executeNonQuery($InsSql, $insParams);
+    }
+   
 
     /**Lista */
 
